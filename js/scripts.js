110d311f5abddb431046bdc7b69a060e8cc0c8f7
Pip Bbaale <namugera.philip@gmail.com>
2020-02-18 10:44:52 +0300

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  $("p").click(function(){
-    $("img").show();
-  });
-});
+  $(".clickable").click(function(){
+    $("#warlus-showing").toggle();
+    $("#warlus-didden").toggle();
+  })
+})
